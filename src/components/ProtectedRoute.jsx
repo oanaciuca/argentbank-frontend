fc1381ee78933f5f1f804c528b95bcd51100c19d
oanaciuca <oana.daniela.ciuca@gmail>
2025-01-23 03:41:24 +0100

    Modification de ProtectedRoute.jsx

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 const ProtectedRoute = ({ children }) => {
   const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
 
+  useEffect(() => {
+    console.log("Vérification de l'authentification dans ProtectedRoute:", isAuthenticated);
+  }, [isAuthenticated]); // Cette dépendance surveille isAuthenticated
+
   if (!isAuthenticated) {
-    return <Navigate to="/sign-in" />;
+    return <Navigate to="/signin" />;
   }
+
   return children; 
 };
 
