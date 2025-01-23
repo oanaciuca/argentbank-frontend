eb119bc1035508fcd8f837ba8b5a8232205ce2d4
oanaciuca <oana.daniela.ciuca@gmail>
2025-01-23 03:41:59 +0100

    Modification de Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
 import Main from '../components/Main';
 
 const Home = () => {
   return (
     <>
-      <Header />
       <Main />
-      <Footer />
     </>
   );
 };
