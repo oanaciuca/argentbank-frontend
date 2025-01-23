6482b2c94d7aafaecd53ddb75ade3823b386196c
oanaciuca <oana.daniela.ciuca@gmail>
2025-01-23 03:41:10 +0100

    Modification de Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { logout } from '../redux/authSlice';
@@ -8,13 +8,8 @@ import argentBankLogo from '../assets/img/argentBankLogo.png';
 const Header = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+ 
   const { isAuthenticated, userData } = useSelector((state) => state.auth);
-
-  useEffect(() => {
-    if (!isAuthenticated) {
-      navigate('/');
-    }
-  }, [isAuthenticated, navigate]);
 
   const handleLogout = () => {
     dispatch(logout());
@@ -36,15 +31,15 @@ const Header = () => {
           <>
             <span className="main-nav-item">
               <i className="fa fa-user-circle"></i>
-              {userData?.firstName || 'User'}
+              {userData ? userData.userName : 'User'}
             </span>
             <button className="main-nav-item" onClick={handleLogout}>
               <i className="fa fa-sign-out"></i>
-              Log out
+              Sign Out
             </button>
           </>
         ) : (
-          <Link className="main-nav-item" to="/sign-in">
+          <Link className="main-nav-item" to="/signin">
             <i className="fa fa-user-circle"></i>
             Sign In
           </Link>
