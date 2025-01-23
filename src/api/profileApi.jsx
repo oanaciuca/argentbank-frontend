310b806d849850c260a98eea8f4201bb904baff0
oanaciuca <oana.daniela.ciuca@gmail>
2025-01-23 03:40:23 +0100

    Modification de profileApi.jsx

--- src/api/profileApi.jsx
+++ src/api/profileApi.jsx
@@ -1,21 +1,16 @@
 export const fetchProfileDetails = async (token) => {
-    try {
-      const response = await fetch('http://localhost:3001/api/v1/user/details', {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${token}`,
-        },
-      });
-  
-      if (!response.ok) {
-        throw new Error('Failed to fetch profile details');
-      }
-  
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error('Error fetching profile details:', error);
-      throw error;
-    }
-  };
+  const response = await fetch('http://localhost:3001/api/v1/user/profile', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  if (!response.ok) {
+    const errorMessage = `Failed to fetch profile details: ${response.status} ${response.statusText}`;
+    throw new Error(errorMessage);
+  }
+
+  return await response.json();
+};
