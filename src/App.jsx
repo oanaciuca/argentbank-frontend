658be503317c9d71a8a9aa7fe7ed15a747b19c72
oanaciuca <oana.daniela.ciuca@gmail>
2025-01-23 03:37:53 +0100

    Modification de App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,29 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import SignIn from './pages/SignIn';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import User from './pages/User';
+import ProtectedRoute from './components/ProtectedRoute';
+
 
 const App = () => {
   return (
       <Router>
+        <Header/>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/sign-in" element={<SignIn />} />
-          
+          <Route path="/signin" element={<SignIn />} />
+          <Route
+          path="/user"
+          element={
+            <ProtectedRoute>
+              <User />
+            </ProtectedRoute>
+          }
+        />
         </Routes>
+        <Footer/>
       </Router>
   );
 };
