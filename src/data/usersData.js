a50c09865be624df9d6fb5f95e1aab290ef5cf11
oanaciuca <oana.daniela.ciuca@gmail>
2025-01-23 03:57:24 +0100

    Ajout du dossier data

---
+++ src/data/usersData.js
@@ -0,0 +1,19 @@
+const usersData = [
+    {
+      firstName: "Tony",
+      lastName: "Stark",
+      email: "tony@stark.com",
+      password: "password123",
+      userName: "Iron",
+    },
+    {
+      firstName: "Steve",
+      lastName: "Rogers",
+      email: "steve@rogers.com",
+      password: "password456",
+      userName: "Captain",
+    },
+  ];
+  
+  export default usersData;
+  
