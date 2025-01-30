f8e49eba24880bb8cb39a3ee930ea51751037e0b
oanaciuca <oana.daniela.ciuca@gmail>
2025-01-30 01:37:01 +0100

    Suppression

--- src/api/profileApi.jsx
+++ src/api/profileApi.jsx
@@ -1,16 +1,36 @@
 export const fetchProfileDetails = async (token) => {
-  const response = await fetch('http://localhost:3001/api/v1/user/profile', {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${token}`,
-    },
-  });
-
-  if (!response.ok) {
-    const errorMessage = `Failed to fetch profile details: ${response.status} ${response.statusText}`;
-    throw new Error(errorMessage);
+  // Vérifie si le token est fourni avant de faire la requête
+  if (!token) {
+    throw new Error('Token manquant');
   }
 
-  return await response.json();
+  try {
+    const response = await fetch('http://localhost:3001/api/v1/user/profile', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,  // Utilisation correcte du token
+      },
+    });
+
+    // Vérifie la réponse de l'API
+    if (!response.ok) {
+      const errorText = await response.text();
+      console.error('Erreur lors de la récupération du profil:', errorText);
+      throw new Error(`Erreur serveur: ${errorText}`);
+    }
+
+    const data = await response.json();
+    console.log('Détails du profil récupérés:', data);  // Ajout du log pour vérifier la réponse
+
+    // Vérifie la structure de la réponse
+    if (!data.status || !data.message || !data.body) {
+      throw new Error('Réponse du serveur invalide : structure de données incorrecte.');
+    }
+
+    return data;
+  } catch (error) {
+    console.error('Erreur lors de la récupération du profil:', error);
+    throw error;  // Lancer l'erreur pour la propager
+  }
 };
