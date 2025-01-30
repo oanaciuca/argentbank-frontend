82fd44bc9f15b05c0cee680a04b9ced07725b0a4
oanaciuca <oana.daniela.ciuca@gmail>
2025-01-30 01:34:07 +0100

    Modifications autjApi.jsx

--- src/api/authApi.jsx
+++ src/api/authApi.jsx
@@ -1,4 +1,4 @@
-import usersData from '../data/usersData'; 
+import usersData from '../data/usersData';
 
 export const login = async (email, password) => {
   const response = await fetch('http://localhost:3001/api/v1/user/login', {
@@ -9,29 +9,35 @@ export const login = async (email, password) => {
     body: JSON.stringify({ email, password }),
   });
 
-  
   if (!response.ok) {
     return Promise.reject('Échec de la connexion');
   }
 
-  
   const data = await response.json();
-  console.log('login response:', data); 
+  console.log('login response:', data);
 
   const user = usersData.find((u) => u.email === email);
   if (!user) {
     throw new Error('Utilisateur introuvable');
   }
 
+  if (!data.body || !data.body.token) {
+    throw new Error('Token manquant dans la réponse');
+  }
+
+  const token = data.body.token;
+  localStorage.setItem('token', `Bearer ${token}`);
+  console.log("Token stocké dans localStorage:", localStorage.getItem('token'));
 
   return {
-    token: data.token, 
+    token,
     userData: {
+      id: user.id, 
       firstName: user.firstName,
       lastName: user.lastName,
       userName: user.userName,
     },
-    email, 
+    email,
   };
 };
 
