1c1dd0bffb823a1c856de7fda565893bbb0abbac
oanaciuca <oana.daniela.ciuca@gmail>
2025-02-15 18:33:18 +0100

    création du fichier

---
+++ src/api/getUserProfile.jsx
@@ -0,0 +1,30 @@
+export const getUserProfile = async () => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      throw new Error('Token absent');
+    }
+  
+    try {
+      const response = await fetch('http://localhost:3001/api/v1/user/profile', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': token,
+        },
+      });
+  
+      if (!response.ok) {
+        const errorText = await response.text();
+        throw new Error(`Erreur serveur: ${errorText}`);
+      }
+  
+      const data = await response.json();
+      // On vérifie si la réponse contient une propriété "user" qui correspond à l'objet complet,
+      // sinon on retourne directement data.
+      return data.user ? data.user : data;
+    } catch (error) {
+      console.error("Erreur lors de la récupération du profil utilisateur:", error);
+      throw error;
+    }
+  };
+  
